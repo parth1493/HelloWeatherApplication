59d937e31b8e8eb02d7fa3fcfda5c62829740545
Parth Desai <parth1493@gmail.com>
2021-06-28 05:30:15 -0400

    DI setup

--- app/src/main/java/com/parth/helloweatherapplication/ui/auth/LauncherFragment.kt
+++ app/src/main/java/com/parth/helloweatherapplication/ui/auth/LauncherFragment.kt
@@ -1,16 +1,19 @@
 package com.parth.helloweatherapplication.ui.auth
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.parth.helloweatherapplication.R
 import com.parth.helloweatherapplication.databinding.FragmentLauncherBinding
 
 class LauncherFragment : BaseAuthFragment() {
 
     private var _binding: FragmentLauncherBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,5 +22,35 @@ class LauncherFragment : BaseAuthFragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_launcher, container, false)
     }
-    
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentLauncherBinding.bind(view)
+
+        Log.d(TAG,"LauncherFragment: ${viewModel.hashCode()}")
+
+
+        binding.register.setOnClickListener {
+            navRegistration()
+        }
+
+        binding.login.setOnClickListener {
+            navLogin()
+        }
+
+        binding.forgotPassword.setOnClickListener { navForgotPassword() }
+
+        binding.focusableView.requestFocus()
+    }
+
+    fun navLogin(){
+        findNavController().navigate(R.id.action_launcherFragment_to_loginFragment2)
+    }
+
+    fun navRegistration(){
+        findNavController().navigate(R.id.action_launcherFragment_to_registerFragment)
+    }
+
+    fun navForgotPassword(){
+        findNavController().navigate(R.id.action_launcherFragment_to_forgotPasswordFragment)
+    }
 }
