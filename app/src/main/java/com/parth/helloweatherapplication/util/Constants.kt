b93b9db8c0f3070719a175ec31a9e07c792d020b
Parth Desai <parth1493@gmail.com>
2021-07-07 04:08:01 -0400

    Added support for network bound resource.

--- app/src/main/java/com/parth/helloweatherapplication/util/Constants.kt
+++ app/src/main/java/com/parth/helloweatherapplication/util/Constants.kt
@@ -7,6 +7,10 @@ class Constants {
         const val BASE_URL = "https://open-api.xyz/api/"
 
         var isNetworkConnected = false
+
+        const val NETWORK_TIMEOUT = 3000L
+        const val TESTING_NETWORK_DELAY = 0L // fake network delay for testing
+        const val TESTING_CACHE_DELAY = 0L // fake cache delay for testing
     }
 
 }
